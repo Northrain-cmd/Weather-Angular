ba655b7cbc956f034542b9a833efb7e0fa70af63
Kris <sichinava.ch@gmail.com>
2020-06-17 17:00:33 +0300

    Geolocation Added

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -14,7 +14,7 @@ export class WeatherService {
         const longitude = position.coords.longitude;
         const latitude = position.coords.latitude;
         this.getWeather('',longitude,latitude);
-      })
+      },error => this.getWeather('New York'))
     }  else {
       console.log("Geolocation not supported")
       this.getWeather('New York');
@@ -24,11 +24,14 @@ export class WeatherService {
     try {
       let response;
       if(longitude) {
+        console.log(latitude,longitude)
          response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=${environment.openWeather}`,
         {mode:'cors'})
-      }
-       response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${environment.openWeather}`,
+      } else {
+        response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${environment.openWeather}`,
       {mode:'cors'});
+      }
+       
       const data = await response.json();
       this.weather.emit(data);
     }
